0a0fd887b182e4f6b115ac021f93e5fbdeed4495
Chidera Ozoh <chideraozoh@icloud.com>
2019-03-30 11:16:59 +0100

    Chore: added in app navigation and styles

--- navigation/AppNav.js
+++ navigation/AppNav.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { createAppContainer, createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation';
-import Deck from '../components/Decks'
+import { createAppContainer, createNavigator, createBottomTabNavigator, createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation';
+import DeckHome from '../components/DeckHome'
 import {  Platform } from 'react-native'
 import AddDeck from '../components/AddDeck'
 import { FontAwesome, Ionicons, Entypo } from '@expo/vector-icons'
+import Decks from '../components/Decks'
+import AddCard from '../components/AddCard'
+import Quiz from '../components/Quiz'
+
 
 const AppNav = Platform.OS === 'ios'?   createBottomTabNavigator({
     Deck: {
-        screen: Deck,
+        screen: DeckHome,
      navigationOptions: {
          tabBarLabel: 'Deck',
          tabBarIcon: ({tintColor}) => <Ionicons name='ios-home' size={30} color={tintColor}/>
@@ -25,7 +29,16 @@ const AppNav = Platform.OS === 'ios'?   createBottomTabNavigator({
   {
   
   navigationOptions: {
-        header: null
+        headerTitle: 'Mobile Flashcards',
+        headerStyle: {
+            backgroundColor: '#335a6b',
+          },
+          headerTintColor: '#f4f4f4',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 20,
+            height: 20
+          },
       },
   tabBarOptions: {
     activeTintColor: '#335a6b',
@@ -45,8 +58,9 @@ const AppNav = Platform.OS === 'ios'?   createBottomTabNavigator({
 })
 :  createMaterialTopTabNavigator({
     Deck: {
-        screen: Deck,
+        screen: DeckHome,
      navigationOptions: {
+         
          tabBarLabel: 'Deck',
          tabBarIcon: () => <Ionicons name='ios-home' size={30} color='#447581'/>
      }
@@ -66,6 +80,7 @@ const AppNav = Platform.OS === 'ios'?   createBottomTabNavigator({
       },
   tabBarOptions: {
     activeTintColor: '#f4f4f4',
+    inactiveTintColor: 'grey',
     style: {
       height: 60,
       backgroundColor:  '#335a6b',
@@ -81,4 +96,44 @@ const AppNav = Platform.OS === 'ios'?   createBottomTabNavigator({
 
 })
 
-export default createAppContainer(AppNav)
+const MainAppNav = createStackNavigator({
+    Home: {
+        screen: AppNav,
+        navigationOptions: {
+         headerBackTitle: null,
+        }
+    },
+    Decks: {
+     screen: Decks,
+    },
+
+    AddCard: {
+     screen: AddCard,
+     navigationOptions: {
+        headerTintColor: '#f4f4f4',
+        headerStyle: {
+          backgroundColor: '#335a6b',
+          height: 40,
+          
+        },
+        title: 'Add Card',
+        headerTintColor: '#f4f4f4'
+      }
+    },
+    Quiz: {
+        screen: Quiz,
+        navigationOptions: {
+            headerTintColor: '#f4f4f4',
+            headerStyle: {
+              backgroundColor: '#335a6b',
+              height: 40,
+              
+            },
+            title: 'Quiz',
+            headerTintColor: '#f4f4f4'
+          }
+    }
+})
+
+
+export default createAppContainer(MainAppNav)
